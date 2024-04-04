9f6222cf404ac79fd682cb87f01a678f042bf1d5
barrosflavio <flavi011@outlook.com>
2024-04-04 17:14:21 -0300

    desafio go 1

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	ebulicao := 373.2
+	ebulicao_celsius := ebulicao - 273
+	fmt.Printf("a agua evapora em %.2fK e em %.2fC", ebulicao, ebulicao_celsius)
+}
